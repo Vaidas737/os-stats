a5b5fc743211ae86052e0507d1ebf47f6980f655
vaidas.k <vkeblys@gamil.com>
2015-05-09 15:21:20 +0300

    work in progress

--- public/d3Models/areaChart/d3AreaChart.js
+++ public/d3Models/areaChart/d3AreaChart.js
@@ -19,14 +19,79 @@ window.d3AreaChart = (function (root) {
   }
 
   /**
+   * Path Object
+   */
+  var Path = function(groupe, options, area, xScale, timeAxis, xAxis, timeScale) {
+
+    var color = options.color;
+
+    var newData = 0;
+    var buffer = generateBuffer(newData);
+
+    var path = null;
+
+    var transition = d3.select({})
+      .transition()
+        .duration(1000)
+        .ease("linear");
+
+    function update() {
+
+      var now = new Date();
+      timeScale.domain([now - 60000, now]);
+
+      buffer.push(newData);
+
+      // redraw the area
+      path
+        .attr("d", area)
+        .attr("transform", null);
+
+      // slide time scale left
+      timeAxis.call(xAxis);
+
+      // slide the are left
+      path
+        .transition()
+          .attr("transform", "translate(" + xScale(0) + ", 0)");
+
+      buffer.shift();
+    }
+
+    function tick() {
+      transition = transition
+        .each(update)
+        .transition()
+          .each("start", tick);
+    }
+
+    this.draw = function() {
+      path = groupe.append("path")
+        .attr("class", "chart-area " + color)
+        .datum(buffer);
+      return this;
+    };
+
+    this.setData = function(nd) {
+      newData = nd;
+      return this;
+    };
+
+    this.startTick = function() {
+      tick();
+      return this;
+    };
+  };
+
+  /**
    * LineChart Object
    */
-  function AreaChart(el) {
+  var AreaChart = function(el) {
 
     var width = el.clientWidth - 20;
     var height = el.clientHeight - 20;
 
-    var period = 60;  // 60sec
+    var period = 60;      // 60sec
     var dimension = 100;  // 100%
 
     var xScale = d3.scale.linear()
@@ -36,6 +101,15 @@ window.d3AreaChart = (function (root) {
     var yScale = d3.scale.linear()
       .range([0, height])
       .domain([0, dimension]);
+
+    var now = new Date();
+    var timeScale = d3.time.scale()
+      .range([0, width])
+      .domain([now - 60000, now]);
+
+    var xAxis = d3.svg.axis()
+      .scale(timeScale)
+      .orient("bottom")
 
     var area = d3.svg.area()
       .x(function (d, i) { return xScale(i); })
@@ -47,71 +121,23 @@ window.d3AreaChart = (function (root) {
       .attr("width", width)
       .attr("height", height);
 
-    var defs = svg.append("defs");
+    var groupe = svg.append("g");
+
+    var defs = groupe.append("defs");
     defs.call(appendClipPath, width, height);
 
-    var groupe = svg.append("g")
-      .attr("clip-path", "url(#clip)");
+    var timeAxis = groupe.append("g")
+      .attr("transform", "translate(0, " + (height - 20) + ")")
+      .call(xAxis);
 
-    //////////////////
-    //    Public    //
-    //////////////////
+    var areaGroupe = groupe.append("g")
+      .attr("clip-path", "url(#clip)")
+      .attr("transform", "translate(0, " + -20 +  ")");
 
     this.appendChart = function (options) {
-      return new Path(groupe, options, area, xScale);
+      return new Path(areaGroupe, options, area, xScale, timeAxis, xAxis, timeScale);
     };
-  }
-
-  /**
-   * Path Object
-   */
-  function Path(groupe, options, area, xScale) {
-
-    var color = options.color;
-
-    var newData = 0;
-    var buffer = generateBuffer(newData);
-
-    var path = null;
-
-    function tick() {
-      buffer.push(newData);
-
-      // update with animation
-      path
-        .attr("d", area)
-        .attr("transform", null)
-        .transition()
-          .duration(1000)
-          .ease("linear")
-          .attr("transform", "translate(" + xScale(0) + ", 0)")
-          .each("end", tick);
-
-      buffer.shift();
-    }
-
-    //////////////////
-    //    Public    //
-    //////////////////
-
-    this.draw = function () {
-      path = groupe.append("path")
-        .attr("class", "chart-area " + color)
-        .datum(buffer)
-        .attr("d", area);
-      return this;
-    };
-
-    this.setData = function (nd) {
-      newData = nd;
-      return this;
-    };
-
-    this.startTick = function () {
-      tick();
-      return this;
-    };
-  }
+  };
 
   return AreaChart;
 })(window);
